4acd54fe3d6a0345c4e1499505714f385949cb99
nayuta-ai <nayuta723@gmail.com>
2023-01-05 02:18:13 +0900

    Add logger

--- libcontainer/nsenter/nsexec.h
+++ libcontainer/nsenter/nsexec.h
@@ -6,12 +6,43 @@
 #include <sched.h>
 #include <setjmp.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
+#include <signal.h>
 
+#define CLONE_PARENT 0x00008000
+
+#define STAGE_SETUP  -1
+#define STAGE_PARENT  0
+#define STAGE_CHILD   1
+#define STAGE_INIT    2
+int current_stage = STAGE_SETUP;
+
+static int syncfd = -1;
+
+/* Assume the stack grows down, so arguments should be above it. */
 struct clone_t {
-  jmp_buf *env;
-  int jmpval;
-  char stack_ptr[1024];  // example stack size
+	/*
+	 * Reserve some space for clone() to locate arguments
+	 * and retcode in this place
+	 */
+	char stack[4096] __attribute__((aligned(16)));
+	char stack_ptr[0];
+
+	/* There's two children. This is used to execute the different code. */
+	jmp_buf *env;
+	int jmpval;
+};
+
+enum sync_t {
+	SYNC_USERMAP_PLS = 0x40,	/* Request parent to map our users. */
+	SYNC_USERMAP_ACK = 0x41,	/* Mapping finished by the parent. */
+	SYNC_RECVPID_PLS = 0x42,	/* Tell parent we're sending the PID. */
+	SYNC_RECVPID_ACK = 0x43,	/* PID was correctly received by parent. */
+	SYNC_GRANDCHILD = 0x44,	/* The grandchild is ready to run. */
+	SYNC_CHILD_FINISH = 0x45,	/* The child or grandchild has finished. */
+	SYNC_MOUNTSOURCES_PLS = 0x46,	/* Tell parent to send mount sources by SCM_RIGHTS. */
+	SYNC_MOUNTSOURCES_ACK = 0x47,	/* All mount sources have been sent. */
 };
 
 static int child_func(void *arg){
@@ -24,8 +55,37 @@ static int clone_parent(jmp_buf *env, int jmpval){
     .env = env,
     .jmpval = jmpval,
   };
-
   return clone(child_func, ca.stack_ptr, CLONE_PARENT | SIGCHLD, &ca);
+}
+
+#define bail(fmt, ...)                      \
+  do {                                      \
+    fprintf(stderr, "FATAL: " fmt ": %m\n", \
+            ##__VA_ARGS__);                 \
+    exit(1);                                \
+  } while(0)
+
+#define LOG_LEVEL_ERROR   1
+#define LOG_LEVEL_WARNING   2
+#define LOG_LEVEL_INFO   3
+#define LOG_LEVEL_DEBUG   4
+
+void log_message(int log_level, const char* message) {
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+
+  char log_level_str[8];
+  switch (log_level) {
+    case LOG_LEVEL_ERROR:   strcpy(log_level_str, "ERROR"); break;
+    case LOG_LEVEL_WARNING: strcpy(log_level_str, "WARNING"); break;
+    case LOG_LEVEL_INFO:    strcpy(log_level_str, "INFO"); break;
+    case LOG_LEVEL_DEBUG:   strcpy(log_level_str, "DEBUG"); break;
+  }
+
+  fprintf(stdout, "[%04d-%02d-%02d %02d:%02d:%02d] [%s] %s\n",
+          tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+          tm.tm_hour, tm.tm_min, tm.tm_sec,
+          log_level_str, message);
 }
 
 #endif  // NEXEC_H
--- libcontainer/tests/nsenter/main.cpp
+++ libcontainer/tests/nsenter/main.cpp
@@ -6,6 +6,11 @@
 #include <unistd.h>
 #include "../../nsenter/nsexec.h"
 
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
 TEST(CloneParentTest, BasicTest) {
   jmp_buf env;
   int jmpval = 10;
@@ -18,11 +23,94 @@ TEST(CloneParentTest, BasicTest) {
   if (setjmp(env) == 0) {
     // setjmp returns 0 the first time it is called
     // code that may cause a longjmp call goes here
-    exit(0);  // exit the child process
   } else {
     // setjmp returns non-zero when longjmp is called
     // code to handle the longjmp goes here
     waitpid(pid, &status, 0);  // wait for the child to exit
     EXPECT_EQ(WEXITSTATUS(status), 0);  // check that the child exited with status 0
   }
+}
+
+
+
+TEST(LogMessageTest, LogsErrorMessage) {
+  // Redirect stdout to a string
+  testing::internal::CaptureStdout();
+
+  log_message(LOG_LEVEL_ERROR, "This is an error message");
+  rewind(stdout);
+
+  char expected_output[1024];
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+  sprintf(expected_output, "[%04d-%02d-%02d %02d:%02d:%02d] [ERROR] This is an error message\n",
+          tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+          tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+  // Get the captured output
+  std::string output = testing::internal::GetCapturedStdout();
+
+  // Verify that the output is correct
+  EXPECT_EQ(output, expected_output);
+}
+
+TEST(LogMessageTest, LogsWarningMessage) {
+  // Redirect stdout to a string
+  testing::internal::CaptureStdout();
+
+  log_message(LOG_LEVEL_WARNING, "This is a warning message");
+  rewind(stdout);
+
+  char expected_output[1024];
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+  sprintf(expected_output, "[%04d-%02d-%02d %02d:%02d:%02d] [WARNING] This is a warning message\n",
+          tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+          tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+  // Get the captured output
+  std::string output = testing::internal::GetCapturedStdout();
+
+  // Verify that the output is correct
+  EXPECT_EQ(output, expected_output);
+}
+
+TEST(LogMessageTest, LogsInfoMessage) {
+  // Redirect stdout to a string
+  testing::internal::CaptureStdout();
+
+  log_message(LOG_LEVEL_INFO, "This is an info message");
+
+  char expected_output[1024];
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+  sprintf(expected_output, "[%04d-%02d-%02d %02d:%02d:%02d] [INFO] This is an info message\n",
+          tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+          tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+  // Get the captured output
+  std::string output = testing::internal::GetCapturedStdout();
+
+  // Verify that the output is correct
+  EXPECT_EQ(output, expected_output);
+}
+
+TEST(LogMessageTest, LogsDebugMessage) {
+  // Redirect stdout to a string
+  testing::internal::CaptureStdout();
+
+  log_message(LOG_LEVEL_DEBUG, "This is a debug message");
+
+  char expected_output[1024];
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+  sprintf(expected_output, "[%04d-%02d-%02d %02d:%02d:%02d] [DEBUG] This is a debug message\n",
+          tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+          tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+  // Get the captured output
+  std::string output = testing::internal::GetCapturedStdout();
+
+  // Verify that the output is correct
+  EXPECT_EQ(output, expected_output);
 }
