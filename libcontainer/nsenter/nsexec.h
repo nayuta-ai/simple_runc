6250658a700473871b1fc42e091e36386284427d
nayuta-ai <nayuta723@gmail.com>
2023-05-03 23:16:43 +0900

    Add write_log and join_namespace function

--- libcontainer/nsenter/nsexec.h
+++ libcontainer/nsenter/nsexec.h
@@ -2,108 +2,82 @@
 #define NEXEC_H
 
 #define _GNU_SOURCE
-#include <errno.h>
 #include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/netlink.h>
 #include <sched.h>
 #include <setjmp.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#include <signal.h>
 
-#include <linux/netlink.h>
-
-#define STAGE_SETUP  -1
-#define STAGE_PARENT  0
-#define STAGE_CHILD   1
-#define STAGE_INIT    2
+#define STAGE_SETUP -1
+#define STAGE_PARENT 0
+#define STAGE_CHILD 1
+#define STAGE_INIT 2
 int current_stage = STAGE_SETUP;
 
 static int syncfd = -1;
 
-#define CLONE_FLAGS_ATTR	27281
+#define INIT_MSG 62000
+#define CLONE_FLAGS_ATTR 27281
+#define NS_PATHS_ATTR 27282
 
-static uint32_t readint32(char *buf)
-{
-	return *(uint32_t *) buf;
-}
+static uint32_t readint32(char *buf) { return *(uint32_t *)buf; }
 
 /* Assume the stack grows down, so arguments should be above it. */
 struct clone_t {
-	/*
-	 * Reserve some space for clone() to locate arguments
-	 * and retcode in this place
-	 */
-	char stack[4096] __attribute__((aligned(16)));
-	char stack_ptr[0];  
+  /*
+   * Reserve some space for clone() to locate arguments
+   * and retcode in this place
+   */
+  char stack[4096] __attribute__((aligned(16)));
+  char stack_ptr[0];
 
-	/* There's two children. This is used to execute the different code. */
-	jmp_buf *env;
-	int jmpval;
+  /* There's two children. This is used to execute the different code. */
+  jmp_buf *env;
+  int jmpval;
 };
 
 enum sync_t {
-	SYNC_USERMAP_PLS = 0x40,	/* Request parent to map our users. */
-	SYNC_USERMAP_ACK = 0x41,	/* Mapping finished by the parent. */
-	SYNC_RECVPID_PLS = 0x42,	/* Tell parent we're sending the PID. */
-	SYNC_RECVPID_ACK = 0x43,	/* PID was correctly received by parent. */
-	SYNC_GRANDCHILD = 0x44,	/* The grandchild is ready to run. */
-	SYNC_CHILD_FINISH = 0x45,	/* The child or grandchild has finished. */
-	SYNC_MOUNTSOURCES_PLS = 0x46,	/* Tell parent to send mount sources by SCM_RIGHTS. */
-	SYNC_MOUNTSOURCES_ACK = 0x47,	/* All mount sources have been sent. */
+  SYNC_USERMAP_PLS = 0x40,  /* Request parent to map our users. */
+  SYNC_USERMAP_ACK = 0x41,  /* Mapping finished by the parent. */
+  SYNC_RECVPID_PLS = 0x42,  /* Tell parent we're sending the PID. */
+  SYNC_RECVPID_ACK = 0x43,  /* PID was correctly received by parent. */
+  SYNC_GRANDCHILD = 0x44,   /* The grandchild is ready to run. */
+  SYNC_CHILD_FINISH = 0x45, /* The child or grandchild has finished. */
+  SYNC_MOUNTSOURCES_PLS =
+      0x46, /* Tell parent to send mount sources by SCM_RIGHTS. */
+  SYNC_MOUNTSOURCES_ACK = 0x47, /* All mount sources have been sent. */
 };
 
 struct nlconfig_t {
-	char *data;
+  char *data;
+  char *namespaces;
+  size_t namespaces_len;
 
-	/* Process settings. */
-	uint32_t cloneflags;
+  /* Process settings. */
+  uint32_t cloneflags;
 };
 
-static int child_func(void *arg){
-    struct clone_t *ca = (struct clone_t *)arg;
-    longjmp(*ca->env, ca->jmpval);
+static int child_func(void *arg) {
+  struct clone_t *ca = (struct clone_t *)arg;
+  longjmp(*ca->env, ca->jmpval);
 }
 
-static int clone_parent(jmp_buf *env, int jmpval){
-    struct clone_t ca = {
-    .env = env,
-    .jmpval = jmpval,
+static int clone_parent(jmp_buf *env, int jmpval) {
+  struct clone_t ca = {
+      .env = env,
+      .jmpval = jmpval,
   };
   return clone(child_func, ca.stack_ptr, CLONE_PARENT | SIGCHLD, &ca);
 }
 
-#define bail(fmt, ...)                      \
-  do {                                      \
-    fprintf(stderr, "FATAL: " fmt ": %m\n", \
-            ##__VA_ARGS__);                 \
-    exit(1);                                \
-  } while(0)
-
-#define LOG_LEVEL_ERROR   1
-#define LOG_LEVEL_WARNING   2
-#define LOG_LEVEL_INFO   3
-#define LOG_LEVEL_DEBUG   4
-
-void write_log(int log_level, const char* message) {
-  time_t t = time(NULL);
-  struct tm tm = *localtime(&t);
-
-  char log_level_str[8];
-  switch (log_level) {
-    case LOG_LEVEL_ERROR:   strcpy(log_level_str, "ERROR"); break;
-    case LOG_LEVEL_WARNING: strcpy(log_level_str, "WARNING"); break;
-    case LOG_LEVEL_INFO:    strcpy(log_level_str, "INFO"); break;
-    case LOG_LEVEL_DEBUG:   strcpy(log_level_str, "DEBUG"); break;
-  }
-
-  fprintf(stdout, "[%04d-%02d-%02d %02d:%02d:%02d] [%s] %s\n",
-          tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
-          tm.tm_hour, tm.tm_min, tm.tm_sec,
-          log_level_str, message);
-}
-
-static int update_uidmap(pid_t pid, char *map, size_t map_len){
+static int update_uidmap(pid_t pid, char *map, size_t map_len) {
   int fd;
   char map_path[1024];
 
@@ -124,7 +98,7 @@ static int update_uidmap(pid_t pid, char *map, size_t map_len){
   return 0;
 }
 
-static int update_gidmap(pid_t pid, char *map, size_t map_len){
+static int update_gidmap(pid_t pid, char *map, size_t map_len) {
   int fd;
   char map_path[1024];
 
@@ -145,80 +119,6 @@ static int update_gidmap(pid_t pid, char *map, size_t map_len){
   return 0;
 }
 
-static int getenv_int(const char *name)
-{
-	char *val, *endptr;
-	int ret;
-
-	val = getenv(name);
-	/* Treat empty value as unset variable. */
-	if (val == NULL || *val == '\0')
-		return -ENOENT;
-
-	ret = strtol(val, &endptr, 10);
-	if (val == endptr || *endptr != '\0')
-		bail("unable to parse %s=%s", name, val);
-	/*
-	 * Sanity check: this must be a non-negative number.
-	 */
-	if (ret < 0)
-		bail("bad value for %s=%s (%d)", name, val, ret);
-
-	return ret;
-}
-
-static void nl_parse(int fd, struct nlconfig_t *config)
-{
-  size_t len, size;
-  struct nlmsghdr hdr;
-  char *current, *data;
-
-  /* Retrieve the netlink header. */
-  len = read(fd, &hdr, NLMSG_HDRLEN);
-  if (len != NLMSG_HDRLEN) bail("invalid netlink header length %zu", len);
-
-  /* Retrieve data. */
-  size = NLMSG_PAYLOAD(&hdr, 0);
-  data = (char *)malloc(size);
-  current = data;
-
-  if (!data)
-		bail("failed to allocate %zu bytes of memory for nl_payload", size);
-  
-  len = read(fd, data, size);
-  if (len != size)
-		bail("failed to read netlink payload, %zu != %zu", len, size);
-  
-  /* Parse the netlink payload. */
-  config->data = data;
-  while (current < data + size) {
-    struct nlattr *nlattr = (struct nlattr *)current;
-    size_t payload_len = nlattr->nla_len - NLA_HDRLEN;
-    
-    /* Advance to payload. */
-    current += NLA_HDRLEN;
-
-    /* Handle payload. */
-    switch (nlattr->nla_type)
-    {
-    case CLONE_FLAGS_ATTR:
-      config->cloneflags = readint32(current);
-      break;
-    
-    default:
-      bail("unknown netlink message type %d", nlattr->nla_type);
-    }
-
-    current += NLA_ALIGN(payload_len);
-  }
-}
-
-
-void nl_free(struct nlconfig_t *config)
-{
-	free(config->data);
-}
-
-// void join_namespace(char *nslist)
+void join_namespace(char *nslist);
 
 #endif  // NEXEC_H
